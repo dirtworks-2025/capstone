4d0ffb06ea2f57c769b0f0799a1aa5686cbce03d
luke-barnes3 <luke03barnes@gmail.com>
2025-03-08 22:02:05 -0500

    feat(controls): tank drive steering with LEDs!

--- arduino/tank_drive_steering/tank_drive_steering.ino
+++ arduino/tank_drive_steering/tank_drive_steering.ino
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 
-const int RIGHT_FORWARD = 2;
-const int RIGHT_BACKWARD = 3;
-const int LEFT_FORWARD = 5;
-const int LEFT_BACKWARD = 6;
+const int RIGHT_FORWARD = 5;
+const int RIGHT_BACKWARD = 6;
+const int LEFT_FORWARD = 2;
+const int LEFT_BACKWARD = 3;
+
+// Joy stick pins
+const int Y_POT = A0;
+const int X_POT= A1;
 
 void setup()
 {
@@ -23,36 +27,62 @@ void setup()
 
 void loop()
 {
-    // Test each motor individually
-    for (int i = 0; i < 255; i += 25)
-    {
-        analogWrite(RIGHT_FORWARD, i);
-        delay(100);
-    }
-    analogWrite(RIGHT_FORWARD, 0);
-    delay(1000);
+    int x_val = analogRead(X_POT);
+    int y_val = analogRead(Y_POT);
 
-    for (int i = 0; i < 255; i += 25)
-    {
-        analogWrite(RIGHT_BACKWARD, i);
-        delay(100);
-    }
-    analogWrite(RIGHT_BACKWARD, 0);
-    delay(1000);
+    // Normalize values
+    float x_norm = map(x_val, 0, 1023, -100, 100) / 100.0;
+    float y_norm = map(y_val, 0, 1023, -100, 100) / 100.0;
 
-    for (int i = 0; i < 255; i += 25)
+    // Set deadzone
+    if (x_norm < 0.1 && x_norm > -0.1)
     {
-        analogWrite(LEFT_FORWARD, i);
-        delay(100);
+        x_norm = 0.0;
     }
-    analogWrite(LEFT_FORWARD, 0);
-    delay(1000);
+    if (y_norm < 0.1 && y_norm > -0.1)
+    {
+        y_norm = 0.0;
+    }
 
-    for (int i = 0; i < 255; i += 25)
+    // Serial.println("X: " + String(x_norm) + " Y: " + String(y_norm));
+    // delay(100);
+
+    // Get speed values
+    float right_speed = (y_norm + x_norm) * (255 / 2);
+    float left_speed = (y_norm - x_norm) * (255 / 2);
+
+    // Serial.println("R: " + String(right_speed) + " L: " + String(left_speed));
+    // delay(100);
+
+    if (right_speed > 0.0)
     {
-        analogWrite(LEFT_BACKWARD, i);
-        delay(100);
+        analogWrite(RIGHT_FORWARD, right_speed);
+        analogWrite(RIGHT_BACKWARD, 0);
     }
-    analogWrite(LEFT_BACKWARD, 0);
-    delay(1000);
+    else if (right_speed < 0.0)
+    {
+        analogWrite(RIGHT_BACKWARD, -right_speed);
+        analogWrite(RIGHT_FORWARD, 0);
+    }
+    else
+    {
+        analogWrite(RIGHT_FORWARD, 0);
+        analogWrite(RIGHT_BACKWARD, 0);
+    }
+
+    if (left_speed > 0.0)
+    {
+        analogWrite(LEFT_FORWARD, left_speed);
+        analogWrite(LEFT_BACKWARD, 0);
+    }
+    else if (left_speed < 0.0)
+    {
+        analogWrite(LEFT_BACKWARD, -left_speed);
+        analogWrite(LEFT_FORWARD, 0);
+    }
+    else
+    {
+        analogWrite(LEFT_FORWARD, 0);
+        analogWrite(LEFT_BACKWARD, 0);
+    }
 }
