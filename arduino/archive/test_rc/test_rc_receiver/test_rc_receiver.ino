b59840bd6ec00b70919578680a075161acef6069
luke-barnes3 <luke03barnes@gmail.com>
2025-03-26 20:52:14 -0400

    wip(controls): tried and failed to get basic RC comms to work... guessing it's a power issue

---
+++ arduino/archive/test_rc/test_rc_receiver/test_rc_receiver.ino
@@ -0,0 +1,23 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+
+void setup()
+{
+    pinMode(53, OUTPUT);
+    Serial.begin(115200);
+    Serial.println("Booted!");
+    if (!radio.begin())
+    {
+        Serial.println(F("radio hardware not responding!"));
+        while (1){} // hold program in infinite loop to prevent subsequent errors
+    }
+}
+
+void loop()
+{
+}
